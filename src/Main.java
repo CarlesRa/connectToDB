178ee1d03c52fcbcf15f5cdcf0158a41f0a31424
carlesRa <jcramos8282@gmail.com>
2019-06-28 20:05:11 +0200

    pruebas superadas, arreglados errores de impresion

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,16 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
         String driver = "jdbc:mysql://localhost:3306/elecciones";
-        try {
-            ConnectToMySQL miConexion = new ConnectToMySQL();
-            miConexion.startConnectionToDB(driver,"root","");
-            miConexion.viewAllTable("partido");
-            System.out.println();
-            miConexion.viewRowsByAttribute("partido","nombre_partido","'pepe'");
-            System.out.println();
-            miConexion.viewRowsByAttribute("partido","nombre_partido","'PP'");
-            System.out.println();
-            miConexion.toWriteAQuery("delete from partido where nombre_partido='PP'");
-            miConexion.viewAllTable("partido");
-            miConexion.closeConnectionToDB();
-        } catch (SQLException e) {
-            System.out.println("Error al conectar");
-            e.printStackTrace();
-        }
+        ConnectToMySQL miConexion = new ConnectToMySQL();
+        miConexion.startConnectionToDB(driver,"root","");
+        miConexion.viewAllTable("partido");
+        System.out.println();
+        miConexion.viewRowsByAttribute("partido","nombre_partido","'PSOE'");
+        System.out.println();
+        miConexion.viewRowsByAttribute("partido","nombre_partido","'PP'");
+        System.out.println();
+        miConexion.toWriteAQuery("delete from partido where nombre_partido='PP'");
+        miConexion.viewAllTable("partido");
+        miConexion.closeConnectionToDB();
     }
 }
