0233514811af5e1e766a63ab1b4f2aff15962b7f
carlesRa <jcramos8282@gmail.com>
2019-06-28 18:18:05 +0200

    funcionen els metodes, exepte el de insertar que dona un error per la sintaxis

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,13 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         try {
             ConnectToMySQL miConexion = new ConnectToMySQL("root","");
+            miConexion.mostrarTodosLosCorredores();
+            System.out.println();
             miConexion.mostrarCorredoresPorNombre("'pepe'");
+            System.out.println();
+            miConexion.mostrarCorredoresPorId("5");
+            System.out.println();
+            miConexion.insertarCorredoresInDB("'Manolo'","120","6","3");
         } catch (SQLException e) {
             System.out.println("Error al conectar");
             e.printStackTrace();
--- src/database/ConnectToMySQL.java
+++ src/database/ConnectToMySQL.java
@@ -12,10 +12,30 @@ public class ConnectToMySQL {
         myStatement = myConnection.createStatement();
     }
 
+    public void mostrarTodosLosCorredores() throws SQLException {
+        myResultset = myStatement.executeQuery("select * from corredores");
+        while (myResultset.next()){
+            System.out.println(myResultset.getString(1) + " " + myResultset.getString(2)+ " " + myResultset.getString(3)+ " " + myResultset.getString(6));
+        }
+    }
     public void mostrarCorredoresPorNombre(String nombre) throws SQLException {
         myResultset = myStatement.executeQuery("select * from corredores where nom="+nombre);
         while (myResultset.next()){
             System.out.println(myResultset.getString(1) + " " + myResultset.getString(2));
+            return;
         }
+        System.out.println("No existe corredores con ese nombre");
+    }
+
+    public void mostrarCorredoresPorId(String corredor_id) throws SQLException{
+        myResultset = myStatement.executeQuery("select * from corredores where corredor_id="+corredor_id);
+        while (myResultset.next()){
+            System.out.println(myResultset.getString(1) + " " + myResultset.getString(2));
+        }
+    }
+
+    public void insertarCorredoresInDB(String nom, String tiempo, String falta1, String falta2) throws SQLException {
+        int resultado;
+        resultado = myStatement.executeUpdate("insert into corredores values(null,"+nom+","+tiempo+","+falta1+","+falta2+",null)");
     }
 }
