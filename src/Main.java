0f4bdc4e321c331b1954a1fd66dc610415ba1b35
carlesRa <jcramos8282@gmail.com>
2019-06-28 19:39:26 +0200

    clase completada lista para utilizar, a falta de nuevas ideas.

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,19 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
+        String driver = "jdbc:mysql://localhost:3306/carreras";
         try {
-            ConnectToMySQL miConexion = new ConnectToMySQL("root","");
-            miConexion.mostrarTodosLosCorredores();
+            ConnectToMySQL miConexion = new ConnectToMySQL();
+            miConexion.startConnectionToDB(driver,"root","");
+            miConexion.viewAllTable("corredores");
             System.out.println();
-            miConexion.mostrarCorredoresPorNombre("'pepe'");
+            miConexion.viewRowsByAttribute("corredores","nom","'pepe'");
             System.out.println();
-            miConexion.mostrarCorredoresPorId("5");
+            miConexion.viewRowsByAttribute("corredores","corredor_id","5");
             System.out.println();
-            miConexion.insertarCorredoresInDB("'Manolo'","120","6","3");
+            miConexion.toWriteAQuery("delete from corredores where nom='martina'");
+            miConexion.viewAllTable("corredores");
+            miConexion.closeConnectionToDB();
         } catch (SQLException e) {
             System.out.println("Error al conectar");
             e.printStackTrace();
--- src/database/ConnectToMySQL.java
+++ src/database/ConnectToMySQL.java
@@ -1,41 +1,60 @@
+/*
+ *@autor Carles Ramos
+ *Class to connect and manage an MySQL DB
+ */
 package database;
 import java.sql.*;
 
 public class ConnectToMySQL {
-    private final String DRIVER = "jdbc:mysql://localhost:3306/carreras";
     private Connection myConnection;
     private Statement myStatement;
     private ResultSet myResultset;
 
-    public ConnectToMySQL(String user, String passWd) throws SQLException {
-        myConnection = DriverManager.getConnection(DRIVER,user,passWd);
-        myStatement = myConnection.createStatement();
+    public ConnectToMySQL(){
+        myConnection = null;
+        myStatement = null;
+        myResultset = null;
     }
 
-    public void mostrarTodosLosCorredores() throws SQLException {
-        myResultset = myStatement.executeQuery("select * from corredores");
-        while (myResultset.next()){
-            System.out.println(myResultset.getString(1) + " " + myResultset.getString(2)+ " " + myResultset.getString(3)+ " " + myResultset.getString(6));
-        }
-    }
-    public void mostrarCorredoresPorNombre(String nombre) throws SQLException {
-        myResultset = myStatement.executeQuery("select * from corredores where nom="+nombre);
-        while (myResultset.next()){
-            System.out.println(myResultset.getString(1) + " " + myResultset.getString(2));
-            return;
-        }
-        System.out.println("No existe corredores con ese nombre");
-    }
-
-    public void mostrarCorredoresPorId(String corredor_id) throws SQLException{
-        myResultset = myStatement.executeQuery("select * from corredores where corredor_id="+corredor_id);
-        while (myResultset.next()){
-            System.out.println(myResultset.getString(1) + " " + myResultset.getString(2));
+    public void startConnectionToDB(String driver, String user, String passWd){
+        try {
+            myConnection = DriverManager.getConnection(driver,user,passWd);
+            myStatement = myConnection.createStatement();
+        } catch (SQLException e) {
+            System.out.println("ERROR: Can't connect to DB");
         }
     }
 
-    public void insertarCorredoresInDB(String nom, String tiempo, String falta1, String falta2) throws SQLException {
-        int resultado;
-        resultado = myStatement.executeUpdate("insert into corredores values(null,"+nom+","+tiempo+","+falta1+","+falta2+",null)");
+    public void closeConnectionToDB(){
+        try {
+            myConnection.close();
+        } catch (SQLException sqle) {
+            System.out.println("ERROR: Can't close connection to DB");
+        }
+        myStatement = null;
+        myResultset = null;
+    }
+    public void viewAllTable(String tableName) throws SQLException {
+        myResultset = myStatement.executeQuery("select * from " + tableName);
+        while(myResultset.next()) {
+            for (int i = 1; i <= myResultset.getMetaData().getColumnCount(); i++) {
+                System.out.print(myResultset.getMetaData().getColumnName(i) + " " + myResultset.getString(i) + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public void viewRowsByAttribute(String tableName, String attribute, String AttributeValue) throws SQLException {
+        myResultset = myStatement.executeQuery("select * from " + tableName + " where " + attribute +"="+AttributeValue);
+        while (myResultset.next()){
+            for (int i = 1; i <= myResultset.getMetaData().getColumnCount(); i++) {
+                System.out.print(myResultset.getMetaData().getColumnName(i) + " " + myResultset.getString(i) + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public void toWriteAQuery(String query) throws SQLException {
+        myStatement.executeUpdate(query);
     }
 }
